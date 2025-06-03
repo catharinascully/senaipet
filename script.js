feb5b93c3114174a41f3c15a0a29bd2359d9e3c2
catharinascully <catharina.scully@gmail.com>
2025-06-03 16:43:36 -0300

    commit 2

--- script.js
+++ script.js
@@ -2,7 +2,9 @@ function Pet(nome, especie, idade){
     this.nome = nome;
     this.especie = especie;
     this.idade = idade;
-    this.falar = function(){ return `O meu nome é ${nome} e sou um ${especie}`};
+
+// criação dos métodos falar e idadeHumana com this
+    this.falar = function(){ return `Oi, eu sou ${this.nome} e sou um ${this.especie}`};
     this.idadeHumana = function(){
         if(this.especie.toLowerCase() == "cachorro"){
             return this.idade * 7
@@ -16,14 +18,35 @@ function Pet(nome, especie, idade){
     };
 }
 
+// cadastros dos pets 
 const meusPets = [
-    new Pet("Bolt", "cachorro", 4),
-    new Pet("Mittens", "gato", 4),
-    new Pet("Rhino", "hamster", 2)
+    new Pet("Bolt", "Cachorro", 4),
+    new Pet("Mittens", "Gato", 4),
+    new Pet("Rhino", "Hamster", 2)
 ];
 
+// listar os animais cadastrados e função anônima atribuída a constante
 const listarAnimais = () => {
     meusPets.forEach(pet => console.log(`Nome: ${pet.nome}, Espécie: ${pet.especie}, Idade: ${pet.idade} anos`))
 };
 
+// buscar pet pelo nome no array
+const buscarPet = function (nome_pet){
+    const encontrado = meusPets.find(pet => pet.nome == nome_pet);
+    
+    if(encontrado){
+        console.log(encontrado.falar());
+        console.log(`Idade humana de ${encontrado.nome}: ${encontrado.idadeHumana()}`);
+        return encontrado
+    }
+    else{
+        console.log(`Pet não encontrado :(`)
+    }
+}
+
+function executarAcoesNosPets(acao){
+    meusPets.forEach(acao)
+}
+
+console.log(buscarPet("Bolt"));
 listarAnimais();
